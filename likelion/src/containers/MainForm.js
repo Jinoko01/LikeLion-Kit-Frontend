f50dc2af89f136a36cab0dd3f0d7eb1e298c66c2
Hwangyongjin <dswvgw6412@naver.com>
2024-02-14 16:06:36 +0900

    style: MainPage Responsive

--- likelion/src/containers/MainForm.js
+++ likelion/src/containers/MainForm.js
@@ -4,28 +4,28 @@ import MainTrack from "../components/main/MainTrack";
 import MainEvents from "../components/main/MainEvents";
 
 const MainBlock = styled.div`
-        min-width: 800px;
-        background-color: #F0F0F0;
-        overflow-x: hidden;
-    `
+  min-width: 290px;
+  background-color: #f0f0f0;
+  overflow-x: hidden;
+`;
 
 const MainCategory = styled.div`
-        font-size: 22px;
-        font-weight: 900;
-        font-style: italic;
-    `
+  font-size: 22px;
+  font-weight: 900;
+  font-style: italic;
+`;
 
 const MainForm = () => {
-    return (
-        <MainBlock>
-            <MainImage></MainImage>
-            <div>
-                <MainTrack mainCategory={MainCategory}></MainTrack>
-                <hr style={{ margin: '0 auto', width: '1200px' }}></hr>
-                <MainEvents mainCategory={MainCategory}></MainEvents>
-            </div>
-        </MainBlock>
-    )
-}
+  return (
+    <MainBlock>
+      <MainImage></MainImage>
+      <div>
+        <MainTrack mainCategory={MainCategory}></MainTrack>
+        <hr style={{ margin: "0 auto", width: "1200px" }}></hr>
+        <MainEvents mainCategory={MainCategory}></MainEvents>
+      </div>
+    </MainBlock>
+  );
+};
 
 export default MainForm;
